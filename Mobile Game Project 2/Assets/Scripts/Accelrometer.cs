c8c7f4a1eb27a255c2530e40918cc2c5b23b35a0
Ray Chng <10988b@gmail.com>
2019-06-11 22:18:17 +0800

    Completes the tilt function
    To change speed of ball, increase mass for increasing speed.
    
    To increase speed of tilting the table, change speed value. Other values do not touch. (They do not do anything special)

---
+++ Mobile Game Project 2/Assets/Scripts/Accelrometer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelrometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody rigid; //for the object rigidbody
+    public int speed =5;
+    public float tiltSpeedLimit = 0.8f;
+    public float tiltLimit = 2;
+    public float tiltSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>(); //for ball
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UseAcclerometer(); //using acclerometer to tilt table
+
+    }
+
+    public void UseAcclerometer()
+    {
+        Vector3 tilt_force = Input.acceleration * speed; //using accleration input to measure tiltness in terms of tilt_force
+
+
+        float tiltX = Mathf.Clamp(tilt_force.x, -tiltSpeedLimit, tiltSpeedLimit); //limits the max tiltspeed in x coordinates;
+        float tiltY = Mathf.Clamp(tilt_force.y, -tiltSpeedLimit, tiltSpeedLimit); //limits the max tiltspeed in y coordinates;
+
+        Debug.Log("Current Power " + transform.rotation.x * tiltSpeed);
+       /* if ( transform.rotation.x >= tiltLimit )
+        {
+            tiltX= 0;
+        }
+        if (transform.rotation.y >= tiltLimit)
+        {
+            tiltY= 0;
+        }*/
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(tiltX, 0, tiltY);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target,  tiltSpeed  );
+        // rigid.AddForce(tiltX * speed, 0,tiltY *speed); //at vector3 in terms of x y axis. because we dw up and down movement of the phone to affect coordinates, hence no z.
+        // transform.Rotate(tiltX, 0, tiltY);
+
+    }
+}
---
+++ Mobile Game Project 2/Assets/Scripts/GyroScope.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroScope : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gyroEnabled = EnableGyro(); //enables gyroscope on phone
+    }
+
+
+    private bool EnableGyro() //this is to ensure gyro is turned on before gameplay.
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotX = gyro.attitude.x; //get X rotation values,  this tills table left and right.
+        float rotY = gyro.attitude.y; //get y rotation values,  this rotates table clockwise and anticlockwise .
+        float rotZ = gyro.attitude.z; //get Z rotation values,  this tills table forward and backwards
+
+        float rotationX = Mathf.Clamp(rotX, -45.0f, 45.0f);
+        float rotationZ = Mathf.Clamp(rotZ, 0.05f, 0.05f);
+      //  print("rotation of x power" + gyro.attitude.x);
+        print("rotation of z " + gyro.attitude.z);
+        if ((int)this.transform.rotation.eulerAngles.x == 45f)
+        {
+            rotX -= 0.5f;
+        }
+        if (345 == (int)this.transform.rotation.eulerAngles.x)
+        {
+            rotX += 0.5f;
+        }
+        /*  if ((int)this.transform.rotation.eulerAngles.z == 45f)
+          {
+              rotZ -= 0.5f;
+          }
+          if (345 == (int)this.transform.rotation.eulerAngles.z)
+          {
+              rotZ += 0.5f;
+          }
+
+
+           if(this.transform.rotation.eulerAngles.x >= 45f )
+           {
+               rotX = 0;
+           }
+           if (this.transform.rotation.eulerAngles.x <= 365-45f)
+               {
+                   rotX = 0;
+               }
+               */
+
+        //print("Statement is " + (365-45f <=this.transform.rotation.eulerAngles.x   || this.transform.rotation.eulerAngles.x >= 45f));
+       // transform.Rotate(0, 0, rotationZ);
+
+      //Quaternion finalrotation  =  Quaternion.Slerp(0, 0, rotationZ); // we dw rotation so only up and down
+
+        //transform.localRotation = finalrotation;
+       
+    }
+}
